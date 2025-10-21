e23168a2fe405d8048a3545f807236d8331fcc7e
codebybilal18 <muhammad.bilal@ocloudsolutions.net>
2025-10-21 13:56:58 +0500

    protected /users/me route using jwt-strategy and guards

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,17 +1,15 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    async validate() {
-
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    async validate(payload: any) {
+        return payload;
     }
 
-    constructor(
-        private config: ConfigService
-    ) {
+    constructor(private config: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('JWT_SECRET')!,
